b7937748c7235da7904eaf7689534a7659f1397a
Eli Mydlarz <eli.mydlarz@gmail.com>
2026-05-23 17:06:03 +0700

    auto(c6d686fc): Let's update to Graphiti 0.29.1 and also implement support for Graphiti'
    File: test/test_helper.exs
    Session: c6d686fc-9b71-469e-a285-0bf38f3aee94
    TranscriptPath: /Users/elimydlarz/.claude/projects/-Users-elimydlarz-projects-gralkor-and-friends-jido-gralkor/c6d686fc-9b71-469e-a285-0bf38f3aee94.jsonl

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,4 +2,4 @@ Gralkor.TestEnv.load(Path.expand("../.env", __DIR__))
 
 {:ok, _} = Gralkor.Client.InMemory.start_link()
 
-ExUnit.start(trace: true, exclude: [:integration, :journey])
+ExUnit.start(trace: true, exclude: [:journey])
