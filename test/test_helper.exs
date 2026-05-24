baa1a725701b0f4ae18497985d4844582474863b
Eli Mydlarz <eli.mydlarz@gmail.com>
2026-05-24 10:41:07 +0700

    auto(80b89c53): > "  1. The existing 25 unit-test failures from the concurrent :gralkor_
    File: test/test_helper.exs
    Session: 80b89c53-0e3f-4f71-8cba-ab35d89ddc29
    TranscriptPath: /Users/elimydlarz/.claude/projects/-Users-elimydlarz-projects-gralkor-and-friends-jido-gralkor/80b89c53-0e3f-4f71-8cba-ab35d89ddc29.jsonl

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,7 @@
 Gralkor.TestEnv.load(Path.expand("../.env", __DIR__))
 
+:ok = Gralkor.Python.ensure_initialised()
+
 {:ok, _} = Gralkor.Client.InMemory.start_link()
 
 ExUnit.start(trace: true, exclude: [:journey])
