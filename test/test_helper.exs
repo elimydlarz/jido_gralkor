5fab3f5c22fb98c08805cc30b769fa8fc383222c
Eli Mydlarz <eli.mydlarz@gmail.com>
2026-05-24 17:04:04 +0700

    auto(9b8f7613): Can we /setup mutation testing? "Muzak"?
    File: test/test_helper.exs
    Session: 9b8f7613-f73b-4134-8641-1c68a469ff8f
    TranscriptPath: /Users/elimydlarz/.claude/projects/-Users-elimydlarz-projects-gralkor-and-friends-jido-gralkor/9b8f7613-f73b-4134-8641-1c68a469ff8f.jsonl

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,6 +2,9 @@ Gralkor.TestEnv.load(Path.expand("../.env", __DIR__))
 
 :ok = Gralkor.Python.ensure_initialised()
 
-{:ok, _} = Gralkor.Client.InMemory.start_link()
+case Gralkor.Client.InMemory.start_link() do
+  {:ok, _} -> :ok
+  {:error, {:already_started, _}} -> :ok
+end
 
 ExUnit.start(trace: true, exclude: [:journey, :functional])
