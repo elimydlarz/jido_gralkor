7907da27987fa25f3226174c97d9a6aa3b54ba02
Eli Mydlarz <eli.mydlarz@gmail.com>
2026-05-26 00:11:01 +0700

    auto(235b676e): I want to use our custom ontologies system to build something like Obser
    File: lib/gralkor/graphiti_pool.ex
    Session: 235b676e-7e66-44fc-968b-dea4df947d81
    TranscriptPath: /Users/elimydlarz/.claude/projects/-Users-elimydlarz-projects-gralkor-and-friends-jido-gralkor/235b676e-7e66-44fc-968b-dea4df947d81.jsonl

--- lib/gralkor/graphiti_pool.ex
+++ lib/gralkor/graphiti_pool.ex
@@ -146,6 +146,8 @@ defmodule Gralkor.GraphitiPool do
                 return x.decode('utf-8') if isinstance(x, (bytes, bytearray)) else x
             for k, v in ontology_dicts.items():
                 kwargs[_dec(k)] = [_dec(i) for i in v] if isinstance(v, list) else v
+        if uuid is not None:
+            kwargs['uuid'] = uuid
         import traceback, sys
         try:
             asyncio._gralkor_run(g.add_episode(**kwargs))
