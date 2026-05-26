e8660ad5f5c8dd978183432583568e43db006535
Eli Mydlarz <eli.mydlarz@gmail.com>
2026-05-26 10:46:03 +0700

    auto(235b676e): I want to use our custom ontologies system to build something like Obser
    File: lib/gralkor/generalise.ex
    Session: 235b676e-7e66-44fc-968b-dea4df947d81
    TranscriptPath: /Users/elimydlarz/.claude/projects/-Users-elimydlarz-projects-gralkor-and-friends-jido-gralkor/235b676e-7e66-44fc-968b-dea4df947d81.jsonl

--- lib/gralkor/generalise.ex
+++ lib/gralkor/generalise.ex
@@ -47,7 +47,7 @@ defmodule Gralkor.Generalise do
     min_confidence = Keyword.get(opts, :min_confidence, @default_min_confidence)
     max_gen_results = Keyword.get(opts, :max_gen_results, @default_max_gen_results)
 
-    gen_partition = "#{group_id}:gen"
+    gen_partition = "#{group_id}_gen"
 
     with {:ok, hypotheses} <- do_hypothesise(transcript, hypothesise_fn, min_confidence),
          :ok <- do_persist(
