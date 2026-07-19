842af344babcc0659790fde8e79bf5f21a88a116
Eli Mydlarz <eli.mydlarz@gmail.com>
2026-07-19 19:02:49 +0700

    auto(019f7676): update test/gralkor/lens/storage/graphiti_test.exs
    Session: 019f7676-6156-75a1-b64f-524818e05663
    Agent: codex

--- test/gralkor/lens/storage/graphiti_test.exs
+++ test/gralkor/lens/storage/graphiti_test.exs
@@ -242,5 +242,18 @@ defmodule Gralkor.Lens.Storage.GraphitiTest do
       assert {:ok, []} = Graphiti.search(store, "launch window", 7, search_fn: search_fn)
       assert_receive {:graph_search, "global", "launch window", 7}
     end
+
+    test "and no originating-Lens filter is supplied" do
+      store = %Store{operator_id: "operator-one", lens: :global}
+      test_pid = self()
+
+      search_fn = fn destination, query, max_results ->
+        send(test_pid, {:graph_search, destination, query, max_results})
+        {:ok, []}
+      end
+
+      assert {:ok, []} = Graphiti.search(store, "launch window", 7, search_fn: search_fn)
+      assert_receive {:graph_search, "global", "launch window", 7}
+    end
   end
 end
