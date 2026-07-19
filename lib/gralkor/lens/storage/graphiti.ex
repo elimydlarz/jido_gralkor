dbfd22a7c3b847250cb116abc8c29f1606261b47
Eli Mydlarz <eli.mydlarz@gmail.com>
2026-07-19 19:55:44 +0700

    auto(019f7676): update lib/gralkor/lens/storage/graphiti.ex
    Session: 019f7676-6156-75a1-b64f-524818e05663
    Agent: codex

--- lib/gralkor/lens/storage/graphiti.ex
+++ lib/gralkor/lens/storage/graphiti.ex
@@ -99,6 +99,11 @@ defmodule Gralkor.Lens.Storage.Graphiti do
     search_fn.(local_destination(operator_id, lens.name), query, max_results)
   end
 
+  def search(%Store{lens: %Lens{scope: :global}}, query, max_results, opts) do
+    search_fn = Keyword.get(opts, :search_fn, &graph_search/3)
+    search_fn.("global", query, max_results)
+  end
+
   def search(%Store{lens: :global}, query, max_results, opts) do
     search_fn = Keyword.get(opts, :search_fn, &graph_search/3)
     search_fn.("global", query, max_results)
