6c6fa976db80904a964129b8c935a27b911b060f
Eli Mydlarz <eli.mydlarz@gmail.com>
2026-08-27 15:38:54 +0700

    auto(01a03e94): update test/functional/reflection_system_functional_test.exs
    Session: 01a03e94-0b4c-7893-9935-ab894a976a34
    Agent: codex

--- test/functional/reflection_system_functional_test.exs
+++ test/functional/reflection_system_functional_test.exs
@@ -842,10 +842,13 @@ defmodule Gralkor.ReflectionSystemFunctionalTest do
                  storage: Gralkor.Reflection.Storage.InMemory
                )
 
-      assert {:ok, [^artefact]} =
-               Store.search([reflection], "operator-one", reflection.name, "durable",
-                 storage: Gralkor.Reflection.Storage.InMemory
-               )
+      assert {:ok, [%{destination: "operator", artefact: ^artefact}]} =
+               Client.search(%Search{
+                 operator_id: "operator-one",
+                 query: "durable",
+                 destinations: [reflection.destination.name],
+                 result_type: :artefacts
+               })
     end
 
     test "and the artefact identifies its declaring Reflection", context do
