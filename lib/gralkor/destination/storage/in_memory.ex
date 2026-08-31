b0d9cc6874740a3d3eea770e6ccc383589e587d0
Eli Mydlarz <eli.mydlarz@gmail.com>
2026-09-01 01:20:01 +0700

    auto(01a05718): update lib/gralkor/destination/storage/in_memory.ex
    Session: 01a05718-5c11-7a72-818e-2549f52e6bec
    Agent: codex

--- lib/gralkor/destination/storage/in_memory.ex
+++ lib/gralkor/destination/storage/in_memory.ex
@@ -3,14 +3,15 @@ defmodule Gralkor.Destination.Storage.InMemory do
   @behaviour Gralkor.Destination.Storage
 
   alias Gralkor.Destination
-  alias Gralkor.Lens.Storage.InMemory
+  alias Gralkor.Lens.Storage.InMemory, as: LensStorage
+  alias Gralkor.Reflection.Storage.InMemory, as: ReflectionStorage
 
   @impl true
   def search(destination, operator_id, _query, :facts, max_results, _opts) do
     results =
       destination
       |> Destination.graph_id(operator_id)
-      |> InMemory.episodes()
+      |> LensStorage.episodes()
       |> Enum.take(max_results)
       |> Enum.map(& &1.content)
 
@@ -18,14 +19,23 @@ defmodule Gralkor.Destination.Storage.InMemory do
   end
 
   def search(destination, operator_id, _query, :episodes, max_results, _opts) do
-    results =
+    lens_episodes =
       destination
       |> Destination.graph_id(operator_id)
-      |> InMemory.episodes()
-      |> Enum.take(max_results)
+      |> LensStorage.episodes()
       |> Enum.map(& &1.content)
 
-    {:ok, results}
+    reflection_episodes =
+      if Process.whereis(ReflectionStorage) do
+        {:ok, artefacts} =
+          ReflectionStorage.search_destination(destination, operator_id, nil, max_results)
+
+        Enum.map(artefacts, &(Jason.encode!(Map.from_struct(&1))))
+      else
+        []
+      end
+
+    {:ok, Enum.take(reflection_episodes ++ lens_episodes, max_results)}
   end
 
   def search(destination, operator_id, _query, :nodes, max_results, opts) do
@@ -34,7 +44,7 @@ defmodule Gralkor.Destination.Storage.InMemory do
     nodes =
       destination
       |> Destination.graph_id(operator_id)
-      |> InMemory.graph()
+      |> LensStorage.graph()
       |> Map.fetch!(:nodes)
       |> Enum.filter(fn node ->
         selected_labels == [] or Enum.any?(node.labels, &(&1 in selected_labels))
