9dd5c3c778352c385c506fe7680cec53c87873ea
Eli Mydlarz <eli.mydlarz@gmail.com>
2026-09-02 17:42:47 +0700

    auto(01a05718): update test/functional/reflection_completion_functional_test.exs
    Session: 01a05718-5c11-7a72-818e-2549f52e6bec
    Agent: codex

--- test/functional/reflection_completion_functional_test.exs
+++ test/functional/reflection_completion_functional_test.exs
@@ -1935,7 +1935,6 @@ defmodule Gralkor.ReflectionCompletionFunctionalTest do
       Pythonx.eval(
         """
         import asyncio
-        group_id = group_id.decode('utf-8') if isinstance(group_id, (bytes, bytearray)) else group_id
         asyncio._gralkor_run(graph.driver.execute_query(
             '''
             MATCH (c:_GralkorEpisodeClaim {uuid: $uuid})
@@ -2039,6 +2038,7 @@ defmodule Gralkor.ReflectionCompletionFunctionalTest do
       Pythonx.eval(
         """
         import asyncio
+        group_id = group_id.decode('utf-8') if isinstance(group_id, (bytes, bytearray)) else group_id
         asyncio._gralkor_run(graph.driver.execute_query(
             '''
             MATCH (c:_GralkorEpisodeClaim {uuid: $uuid})
