0cc24ad7bfd07a930eff2e6eb43ec208fbce3578
Eli Mydlarz <eli.mydlarz@gmail.com>
2026-09-02 16:09:26 +0700

    auto(01a060d3): update lib/gralkor/destination/storage/graphiti.ex (+2 more)
    Session: 01a060d3-34eb-7cf0-87e4-87d08092bdd4
    Agent: codex

--- lib/gralkor/destination/storage/graphiti.ex
+++ lib/gralkor/destination/storage/graphiti.ex
@@ -3,27 +3,69 @@ defmodule Gralkor.Destination.Storage.Graphiti do
   @behaviour Gralkor.Destination.Storage
 
   alias Gralkor.Destination
+  alias Gralkor.Artefact
   alias Gralkor.Format
   alias Gralkor.GraphitiPool
 
   @impl true
-  def put_artefact(output, reflection_name, operator_id, artefact) do
-    Gralkor.Reflection.Storage.Graphiti.put_output(
-      output,
-      reflection_name,
-      operator_id,
-      artefact
-    )
+  def put_artefact(output, reflection_name, operator_id, %Artefact{} = artefact) do
+    put_artefact(output, reflection_name, operator_id, artefact, fn group_id,
+                                                                    content,
+                                                                    source_description,
+                                                                    ontology,
+                                                                    opts ->
+      GraphitiPool.add_episode(
+        GraphitiPool,
+        group_id,
+        content,
+        source_description,
+        ontology,
+        opts
+      )
+    end)
+  end
+
+  @doc false
+  def put_artefact(output, reflection_name, operator_id, %Artefact{} = artefact, add_episode) do
+    case add_episode.(
+           Destination.graph_id(output.destination, operator_id),
+           Jason.encode!(Map.from_struct(artefact)),
+           "reflection:#{reflection_name}",
+           output.ontology,
+           uuid: artefact.id
+         ) do
+      {:error, {:episode_conflict, id}} -> {:error, {:artefact_conflict, id}}
+      outcome -> outcome
+    end
   end
 
   @impl true
   def get_artefact(output, reflection_name, operator_id, artefact_id) do
-    Gralkor.Reflection.Storage.Graphiti.get_output(
-      output,
-      reflection_name,
-      operator_id,
-      artefact_id
-    )
+    get_artefact(output, reflection_name, operator_id, artefact_id, &GraphitiPool.get_episode/2)
+  end
+
+  @doc false
+  def get_artefact(output, _reflection_name, operator_id, artefact_id, get_episode) do
+    case get_episode.(Destination.graph_id(output.destination, operator_id), artefact_id) do
+      {:ok, episode} ->
+        case decode_artefact(episode) do
+          [%Artefact{id: ^artefact_id} = artefact] ->
+            if extraction_complete?(episode) do
+              {:ok, artefact}
+            else
+              {:error, {:incomplete_artefact, artefact}}
+            end
+
+          [%Artefact{}] ->
+            {:error, {:artefact_conflict, artefact_id}}
+
+          [] ->
+            {:error, {:invalid_artefact, artefact_id}}
+        end
+
+      {:error, _} = error ->
+        error
+    end
   end
 
   @impl true
@@ -75,7 +117,7 @@ defmodule Gralkor.Destination.Storage.Graphiti do
       {:ok, episodes} ->
         artefacts =
           episodes
-          |> Enum.flat_map(&Gralkor.Reflection.Storage.Graphiti.decode/1)
+          |> Enum.flat_map(&decode_artefact/1)
           |> filter_artefact(Keyword.get(opts, :artefact_id))
 
         converge_artefacts(artefacts, max_results)
@@ -118,4 +160,24 @@ defmodule Gralkor.Destination.Storage.Graphiti do
 
   defp reflection_episode(content, source_description),
     do: %{content: content, source_description: source_description}
+
+  @doc false
+  def decode_artefact(%{content: content}), do: decode_artefact(content)
+  def decode_artefact(%{"content" => content}), do: decode_artefact(content)
+
+  def decode_artefact(content) when is_binary(content) do
+    case Jason.decode(content) do
+      {:ok, %{"id" => id, "payload" => payload} = decoded} when map_size(decoded) == 2 ->
+        [%Artefact{id: id, payload: payload}]
+
+      _ ->
+        []
+    end
+  end
+
+  def decode_artefact(_), do: []
+
+  defp extraction_complete?(episode) do
+    Map.get(episode, :extraction_complete, Map.get(episode, "extraction_complete", false)) == true
+  end
 end
