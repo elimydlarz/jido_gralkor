8074bfb1c66c4ac46a5da52b1db4a4f1804b74c1
Eli Mydlarz <eli.mydlarz@gmail.com>
2026-09-02 16:01:55 +0700

    auto(01a060d3): update lib/gralkor/reflection/supervisor.ex (+1 more)
    Session: 01a060d3-34eb-7cf0-87e4-87d08092bdd4
    Agent: codex

--- lib/gralkor/reflection/supervisor.ex
+++ lib/gralkor/reflection/supervisor.ex
@@ -4,7 +4,6 @@ defmodule Gralkor.Reflection.Supervisor do
   use Supervisor
 
   alias Gralkor.Reflection.Scheduler
-  alias Gralkor.Reflection.Schedule
 
   def start_link(opts \\ []) do
     Supervisor.start_link(__MODULE__, opts, name: Keyword.get(opts, :name, __MODULE__))
@@ -13,10 +12,7 @@ defmodule Gralkor.Reflection.Supervisor do
   @impl true
   def init(opts) do
     scheduler_opts = Keyword.get(opts, :scheduler_opts, [])
-    schedule_opts = Keyword.get(opts, :schedule_opts, [])
 
-    Supervisor.init([{Scheduler, scheduler_opts}, {Schedule, schedule_opts}],
-      strategy: :one_for_one
-    )
+    Supervisor.init([{Scheduler, scheduler_opts}], strategy: :one_for_one)
   end
 end
