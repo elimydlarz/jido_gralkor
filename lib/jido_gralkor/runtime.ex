281b306322b757d996d4a1db811b86e05f37bad1
Eli Mydlarz <eli.mydlarz@gmail.com>
2026-09-04 15:56:20 +0700

    auto(01a06b31): update lib/jido_gralkor/runtime.ex
    Session: 01a06b31-be77-7a21-abf1-d13cb1d244a7
    Agent: codex

--- lib/jido_gralkor/runtime.ex
+++ lib/jido_gralkor/runtime.ex
@@ -8,7 +8,10 @@ defmodule JidoGralkor.Runtime do
     GenServer.start_link(__MODULE__, opts, name: via(owner))
   end
 
-  def snapshot(owner), do: GenServer.call(via(owner), :snapshot)
+  def snapshot(owner) do
+    ensure_started(owner)
+    GenServer.call(via(owner), :snapshot)
+  end
 
   @impl GenServer
   def init(opts) do
@@ -21,6 +24,17 @@ defmodule JidoGralkor.Runtime do
 
   @impl GenServer
   def handle_call(:snapshot, _from, state), do: {:reply, state.configuration, state}
+
+  defp ensure_started(owner) do
+    case :global.whereis_name({__MODULE__, owner}) do
+      :undefined ->
+        {:ok, _state} = Jido.AgentServer.state(owner)
+        :ok
+
+      _pid ->
+        :ok
+    end
+  end
 
   defp via(owner), do: {:global, {__MODULE__, owner}}
 end
