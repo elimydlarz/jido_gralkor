eb9bb2665ac599d6579ef54ecd9f8d385fb47ac4
Eli Mydlarz <eli.mydlarz@gmail.com>
2026-09-04 17:38:54 +0700

    auto(01a06b31): delete lib/gralkor/artefact/return_handler.ex (+3 more)
    Session: 01a06b31-be77-7a21-abf1-d13cb1d244a7
    Agent: codex

--- lib/gralkor/artefact/return_handler.ex
+++
@@ -1,6 +0,0 @@
-defmodule Gralkor.Artefact.ReturnHandler do
-  @moduledoc "Consumer callback contract for delivering a Reflection artefact."
-
-  @callback return(String.t(), String.t(), Gralkor.Artefact.t()) ::
-              :ok | {:error, term()}
-end
