c2d804a188e541c52248d90073b730512c48b6f4
Eli Mydlarz <eli.mydlarz@gmail.com>
2026-09-05 23:11:35 +0700

    auto(01a06b31): update test/jido_gralkor/plugin_test.exs
    Session: 01a06b31-be77-7a21-abf1-d13cb1d244a7
    Agent: codex

--- test/jido_gralkor/plugin_test.exs
+++ test/jido_gralkor/plugin_test.exs
@@ -114,7 +114,7 @@ defmodule JidoGralkor.PluginTest do
       assert %{
                start:
                  {JidoGralkor.Runtime, :start_link,
-                  [[configuration: %{destinations: [], lenses: [], reflections: []}]]}
+                  [[owner: _owner, configuration: %{destinations: [], lenses: [], reflections: []}]]}
              } = Plugin.child_spec(%{})
     end
   end
