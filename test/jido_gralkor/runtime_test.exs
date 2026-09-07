a38a6f9ea318d11b8e9e57777f5f8fc44c491f29
Eli Mydlarz <eli.mydlarz@gmail.com>
2026-09-07 17:16:19 +0700

    auto(01a06b31): update test/jido_gralkor/runtime_test.exs
    Session: 01a06b31-be77-7a21-abf1-d13cb1d244a7
    Agent: codex

--- test/jido_gralkor/runtime_test.exs
+++ test/jido_gralkor/runtime_test.exs
@@ -78,7 +78,7 @@ defmodule JidoGralkor.RuntimeTest do
                  invocation("async-invocation"),
                  &send(test_pid, {:reflection_callback, &1}),
                  run_reflection: fn _reflection, _invocation, _opts ->
-                   send(test_pid, :production_started)
+                   send(test_pid, {:production_started, self()})
                    receive do
                      :release -> {:ok, Gralkor.Artefact.new("async", %{})}
                    end
@@ -88,9 +88,9 @@ defmodule JidoGralkor.RuntimeTest do
                  end
                )
 
-      assert_receive :production_started
+      assert_receive {:production_started, worker}
       refute_receive {:reflection_callback, _}
-      send(test_pid, :release)
+      send(worker, :release)
       assert_receive {:reflection_callback, %{outcome: :delivered}}
     end
   end
@@ -360,7 +360,7 @@ defmodule JidoGralkor.RuntimeTest do
                  end
                )
 
-      assert_receive {:reflection, "review"}
+      assert_receive {:reflection, "reviews"}
       assert :ok = Runtime.replace(self(), replacement_configuration("new"))
       send(parent, :release)
     end
