d2597445159f5326ec4caa96240cb594d9dec8fb
Eli Mydlarz <eli.mydlarz@gmail.com>
2026-09-07 17:15:57 +0700

    auto(01a06b31): update test/gralkor/reflection/runner_test.exs
    Session: 01a06b31-be77-7a21-abf1-d13cb1d244a7
    Agent: codex

--- test/gralkor/reflection/runner_test.exs
+++ test/gralkor/reflection/runner_test.exs
@@ -7,8 +7,6 @@ defmodule Gralkor.Reflection.RunnerTest do
   alias Gralkor.Reflection
   alias Gralkor.Reflection.ChainOfThought
   alias Gralkor.Reflection.ChainOfThought.Step
-  alias Gralkor.Reflection.Runner, as: ProductionRunner
-
   defmodule Runner do
     def run(reflection, invocation, opts \\ []) do
       Gralkor.Reflection.Runner.run(
