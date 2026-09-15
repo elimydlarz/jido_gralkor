bdaa9c97897af7c42e0644e03c406f9de9812e8b
Eli Mydlarz <eli.mydlarz@gmail.com>
2026-09-15 14:28:57 +0700

    auto(01a09f7d): update test/functional/personal_graph_migration_functional_test.exs
    Session: 01a09f7d-ac9e-76f1-a36e-50be3c137feb
    Agent: codex

--- test/functional/personal_graph_migration_functional_test.exs
+++ test/functional/personal_graph_migration_functional_test.exs
@@ -1352,7 +1352,8 @@ defmodule Gralkor.PersonalGraphMigrationFunctionalTest do
         server = Redis(dbfilename=path.decode(), serverconfig={'port': '0'})
         server.config_set('loglevel', 'warning')
         server.config_set('save', '')
-        connection = {'unix_socket_path': server.socket_file}
+        import os
+    connection = {'unix_socket_path': os.path.realpath(server.socket_file)}
         if password is not None:
             secret = password.decode()
             server.execute_command('ACL', 'SETUSER', 'migration-fixture', 'on', '>' + secret, '~*', '+@all')
