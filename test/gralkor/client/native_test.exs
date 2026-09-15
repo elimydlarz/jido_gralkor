1bdb963faaabdee14434e8c254e875ed6dbe4159
Eli Mydlarz <eli.mydlarz@gmail.com>
2026-09-15 12:13:07 +0700

    auto(01a09f7d): update test/gralkor/client/native_test.exs
    Session: 01a09f7d-ac9e-76f1-a36e-50be3c137feb
    Agent: codex

--- test/gralkor/client/native_test.exs
+++ test/gralkor/client/native_test.exs
@@ -652,18 +652,21 @@ defmodule Gralkor.Client.NativeTest do
       )
 
     pid =
-      start_supervised!({GraphitiPool, [
-        name: Gralkor.GraphitiPool,
-        table: :gralkor_graphiti_instances,
-        falkordb_spec: {:embedded, "/tmp/never_used"},
-        construct_falkor_db: fn _spec -> :stub_falkor_db end,
-        construct_shared_clients: fn _llm, _embedder ->
-          %{llm_client: nil, embedder: nil, cross_encoder: nil}
-        end,
-        construct_instance: fn _db, _shared, _group_id -> g end,
-        warmup: false,
-        install_loop_fn: &Gralkor.Python.install_async_runtime/0
-      ]})
+      start_supervised!(
+        {GraphitiPool,
+         [
+           name: Gralkor.GraphitiPool,
+           table: :gralkor_graphiti_instances,
+           falkordb_spec: {:embedded, "/tmp/never_used"},
+           construct_falkor_db: fn _spec -> :stub_falkor_db end,
+           construct_shared_clients: fn _llm, _embedder ->
+             %{llm_client: nil, embedder: nil, cross_encoder: nil}
+           end,
+           construct_instance: fn _db, _shared, _group_id -> g end,
+           warmup: false,
+           install_loop_fn: &Gralkor.Python.install_async_runtime/0
+         ]}
+      )
 
     %{pool: pid, g: g}
   end
